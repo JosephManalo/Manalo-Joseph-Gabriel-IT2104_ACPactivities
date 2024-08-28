72cfc553880ff41bbf6b3435a258980d0f8ab13f
JosephManalo <josephmanalo1221@gmail.com>
2024-08-28 12:44:19 +0800

    Add files via upload

---
+++ Exercise Set 2/string-handling.py
@@ -0,0 +1,4 @@
+1stcolor = input ("Enter first color: ")
+2ndcolor = input ("Enter second color: ")
+3rdcolor = input ("Enter third color: ")
+print (f"{1stcolor}, {2ndcolor} and {3rdcolor} created a new color!")
