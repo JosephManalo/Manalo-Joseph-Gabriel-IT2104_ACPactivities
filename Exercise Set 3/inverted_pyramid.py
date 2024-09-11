26c064ba3977270aeab50f2275ae4c90aa94b116
JosephManalo <josephmanalo1221@gmail.com>
2024-09-11 11:36:46 +0800

    Delete Exercise Set 3 directory

--- Exercise Set 3/inverted_pyramid.py
+++
@@ -1,7 +0,0 @@
-def pyramid(height):
-    for i in range(height, 0, -1):
-        print('*' * i)
-
-# Example usage
-height = int(5)
-pyramid(height)
--- Exercise Set 3/sliced_tuple.py
+++
@@ -1,2 +0,0 @@
-multiples_of_9 = tuple(i for i in range(9, 91, 9))
-print("Sliced Tuple:", multiples_of_9[5:8])
