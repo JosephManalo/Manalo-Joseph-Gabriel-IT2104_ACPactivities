007191f46b89f5ecbe58a5a7db3f94f897bbc9c2
JosephManalo <josephmanalo1221@gmail.com>
2024-09-11 11:34:49 +0800

    Delete Exercise 3 directory

--- Exercise 3/vowels_list.py
+++
@@ -1,6 +0,0 @@
-def string(s):
-    vowels = 'aeiouAEIOU'
-    return [char for char in s if char in vowels]
-input_string = input("Enter a string: ")
-vowel_list = string(input_string)
-print(vowel_list)
