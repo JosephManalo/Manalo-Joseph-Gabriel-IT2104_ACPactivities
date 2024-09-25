78d2f6d2edad61bea35f5f7d29f5eb312795b001
JosephManalo <josephmanalo1221@gmail.com>
2024-09-25 11:50:03 +0800

    Delete Exercise Set 2 directory

--- Exercise Set 2/bitwise2.py
+++
@@ -1,3 +0,0 @@
-n = int(input("Enter an integer value for n: "))
-result = n & 30
-print(f"{n} AND 30 = {result}")
--- Exercise Set 2/typecasting.py
+++
@@ -1,5 +0,0 @@
-x = int(input("Enter an integer: "))
-y = float(input("Enter a float: "))
-convertedx = float(x)
-difference = convertedx - y
-print(f"Difference: {difference:.2f}")
